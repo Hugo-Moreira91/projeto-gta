00b398774c9e45dda03736a8f03882a8d744cd17
Hugo-Moreira91 <hugo.cesar91@gmail.com>
2024-01-27 22:22:44 -0300

    Projeto finalizado

---
+++ src/js/scripts.js
@@ -0,0 +1,43 @@
+/*
+
+Objetivo 1 - quando o usuário clicar no botão de seleção de plataformas deve abrir uma caixa com os botões de seleção de plataforma
+    Passo 1 - pegar o botão de seleção de plataformas no JS pra poder verificar quando o usuário clicar em cima dele
+
+    Passo 2 - pegar o elemento do conteudo que precisa ser mostrado
+
+    Passo 3 - pegar o clique do usuario no js
+
+    Passo 4 - quando o usuário clicar, adicionar a classe ativo na listagem de plataformas dentro do botão pra que o conteúdo apareça
+
+
+Objetivo 2 - caso a lista de botões de plataformas já esteja aparecendo e o usuário clicar em cima do botão, o conteúdo deve ser escondido
+
+    Passo 1 - verificar se o botão já esta aberto, se sim, devemos remover a classe ativo pra que ele esconda o conteúdo novamente
+
+*/
+
+// Objetivo 1 - quando o usuário clicar no botão de seleção de plataformas deve abrir uma caixa com os botões de seleção de plataforma
+
+// Passo 1 - pegar o botão de seleção de plataformas no JS pra poder verificar quando o usuário clicar em cima dele
+const botao = document.querySelector(".btn-plataformas");
+
+// Passo 2 - pegar o elemento do conteudo que precisa ser mostrado
+const elementoPlataformas = document.querySelector(".btn-plataformas .plataformas");
+
+// Passo 3 - pegar o clique do usuario no js
+botao.addEventListener('click', () => {
+    // Objetivo 2 - caso a lista de botões de plataformas já esteja aparecendo e o usuário clicar em cima do botão, o conteúdo deve ser escondido
+
+    // // Passo 1 - verificar se o botão já esta aberto, se sim, devemos remover a classe ativo pra que ele esconda o conteúdo novamente
+    // if(elementoPlataformas.classList.contains('ativo')){
+    //     elementoPlataformas.classList.remove('ativo');
+    // }else{
+    //     // Passo 4 - quando o usuário clicar, adicionar a classe ativo na listagem de plataformas dentro do botão pra que o conteúdo apareça
+    //     elementoPlataformas.classList.add('ativo');
+    // }
+
+    // Refatorando o código acima (do if-else)
+    // Refatorar é reduzir código, sem perder a lógica desejada
+    // Na refatoração abaixo, elementoPlataformas recebe e retira a classe ativo, através do método toggle
+    elementoPlataformas.classList.toggle('ativo');
+});
